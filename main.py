3c628686344520ed8a99db8f3e763fb3650c09a0
floria914 <floriawu914@gmail.com>
2023-08-30 20:54:25 +0200

    Cerate columns, image, and descriptions

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/myphoto.png",width=300)
+
+with col2:
+    st.title("Nan Wu")
+    content = "Hi, I am Nan! I am a fast learner and a enthusiastic programmer who is always curious"\
+              " and enjoys solving interesting problems.I graduated in 2023 with a Master of Science"\
+              " in Visual Computing from Saarland University in Germany with a focus on Machine Learning, AI, and Neuroscience areas."
+    st.info(content)

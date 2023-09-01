6e9c33bc4fbf37443d37622baaa87318da76cab2
floria914 <floriawu914@gmail.com>
2023-09-01 12:12:48 +0200

    Add email

---
+++ sent_email.py
@@ -0,0 +1,21 @@
+import smtplib, ssl
+
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "floriawu914@gmail.com"
+# type your app password
+password = " "
+
+receiver = "floriawu914@gmail.com"
+context = ssl.create_default_context()
+
+message="""\
+Subject:Hello! 
+How are you? 
+Bye!"""
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
